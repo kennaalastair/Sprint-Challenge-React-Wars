2f837a3f04b73ba69cd91dc415715dcf45f04df2
Kenna <kennaalastair@gmail.com>
2019-07-19 10:19:57 -0700

    getting data from star wars api

---
+++ starwars/src/components/CharactersCard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const CharactersCard = (props) => {
+    return (
+        <div className="character-card">
+            <p>Star Wars Characters</p>
+            <div className="char-names"></div>
+        </div>
+    );
+}
+
+export default CharactersCard;
